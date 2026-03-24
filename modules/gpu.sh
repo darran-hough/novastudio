071ef0666a3b9cbdd972dc87ff6a7487db300d49
Darran Hough <115110194+darran-hough@users.noreply.github.com>
2026-03-24 13:57:51 +0000

    Update gpu.sh

--- modules/gpu.sh
+++ modules/gpu.sh
@@ -6,6 +6,9 @@ log "Configuring GPU drivers"
 case "$GPU_VENDOR" in
 
 nvidia)
+sudo dnf install \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 install_pkg akmod-nvidia xorg-x11-drv-nvidia-cuda
 ;;
 
